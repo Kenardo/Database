64ef04d41d6183aeac2cdf76469d25f0a3c16f11
stewiejnr <stewie920@gmail.com>
2014-04-09 23:07:39 -0500

    dummy data for users added

--- dummydata.sql
+++ dummydata.sql
@@ -1,11 +1,11 @@
 /* dummy data for users table */
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i hate burger','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i feel fat today','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i am bored','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i need a wife','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i am sick','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i am lonely','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i am getting married','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('','','');
-INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('','','');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i hate burger','1','abettor15');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i feel fat today','2','abnormalize67');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i am bored','3','special45');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i need a wife','4','intent90');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i am sick','5','helloworld10');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i am lonely','6','thisisme86');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('i am getting married','7','hehelol43');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('woot','8','gestureme53');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('helloworld','9','yummy12');
+INSERT INTO `users`(`user_status`, `reg_id`, `password`) VALUES ('rihanna is awesome','10','hamburger20');
