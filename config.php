d98024cdff9525ad8217588f28992fcb3a52df8e
stewiejnr <stewie920@gmail.com>
2014-04-12 19:49:30 -0500

    ....

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	$con = mysql_connect('localhost','root',''); /*connects to the database */
+	mysql_select_db('aarkdb'); /* selects the mysql database named 'coursemgmtdb' */
+	
+	$query = 'SELECT * FROM courses';  /* sql query string being assigned to php variable */
+	$result = mysql_query($query) or die("Query failed" . mysql_error()); /*queries the database with the query string in variable ($query), returns an error if the query fails */
+	
+?>
