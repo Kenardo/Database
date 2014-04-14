95d61925b87d6ff043cc4ab22a964a0aa282e023
stewiejnr <stewie920@gmail.com>
2014-04-14 18:26:16 -0500

    created all tables from 3NF on google drive

--- schemagroup.sql
+++ schemagroup.sql
@@ -29,41 +29,16 @@ foreign key (userid) references users(userid) on update cascade on delete cascad
 
 create table profile(
 userid int,
-fname varchar(50),
-lname varchar(50),
-email varchar(100),
-profile_pic varchar(128),
 user_status varchar(100),
-dob date,
+profile_pic varchar(128),
 primary key(userid),
 foreign key (userid) references users(userid) on update cascade on delete cascade
 );
 
 create table groups(
 groupid int auto_increment not null,
-groupname varchar(50),
+groupname varchar(50),a
 primary key(groupid)
-);
-
-create table posts(
-postid int auto_increment not null,
-title varchar(128),
-likes int,
-image_path varchar(128),
-g_post_type varchar(50),
-text_body varchar(200),
-post_likes int,
-primary key (postid)
-);
-
-create table group_posts(
-gpostid int auto_increment not null,
-title varchar(128),
-g_image_path varchar(128),
-g_post_type varchar(50),
-text_body varchar(200),
-gpost_likes int,
-primary key (gpostid)
 );
 
 create table comments(
@@ -73,14 +48,58 @@ comment_likes int,
 primary key(commentid)
 );
 
+create table comment_likes(
+commentid int,
+userid int,
+primary key (commentid, userid),
+foreign key (userid) references users(userid) on update cascade on delete cascade,
+foreign key (commentid) references comments(commentid) on update cascade on delete cascade
+);
 
-create table friend_of(
-friend int,
-friend_owner int,
-type varchar(50),
-primary key(friend, friend_owner),
-foreign key (friend) references users(userid) on update cascade on delete cascade,
-foreign key (friend_owner) references users(userid) on update cascade on delete cascade
+create table post(
+postid int auto_increment not null,
+title varchar(128),
+post_type varchar(50),
+primary key (postid)
+);
+
+create table post_likes(
+userid int,
+postid int,
+primary key (userid, postid),
+foreign key (userid) references users(userid) on update cascade on delete cascade,
+foreign key (postid) references post(postid) on update cascade on delete cascade
+
+create table post_content(
+postid int,
+image_path varchar(128),
+text_body varchar(250),
+post_like int,
+primary key (postid),
+foreign key (postid) references post(postid) on update cascade on delete cascade
+);
+
+create table group_post(
+gpostid int auto_increment not null,
+title varchar(128),
+g_post_type varchar(50),
+primary key (gpostid)
+);
+
+create table gpost_content(
+gpostid int,
+g_image_path varchar(128),
+text_body varchar(250),
+gpost_like int,
+primary key (gpostid),
+foreign key (gpostid) references group_post(gpostid) on update cascade on delete cascade
+);
+
+create table gpost_likes(
+userid int,
+gpostid int,
+primary key(userid,gpostid),
+foreign key (gpostid) references group_post(gpostid) on update cascade on delete cascade
 );
 
 create table creates(
@@ -92,12 +111,21 @@ foreign key (userid) references users(userid) on update cascade on delete cascad
 foreign key (postid) references posts(postid) on update cascade on delete cascade
 );
 
+create table friend_of(
+friend int,
+friend_owner int,
+type varchar(50),
+primary key(friend, friend_owner),
+foreign key (friend) references users(userid) on update cascade on delete cascade,
+foreign key (friend_owner) references users(userid) on update cascade on delete cascade
+);
+
 create table comments_on(
 userid int,
 postid int,
 commentid int,
 date_commented date,
-primary key(userid),
+primary key(userid, postid, commentid),
 foreign key (userid) references users(userid) on update cascade on delete cascade,
 foreign key (postid) references posts(postid) on update cascade on delete cascade
 );
