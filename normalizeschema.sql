024cbc54ed6d684f3696a0ed80128792141cf947
stewiejnr <stewie920@gmail.com>
2014-04-14 20:50:55 -0500

    final update for schema

--- normalizeschema.sql
+++ normalizeschema.sql
@@ -1,84 +1,99 @@
-
-
-create table users(
-userid int auto_increment not null,
-fname varchar(50),
-lname varchar(50),
-primary key(userid)
-);
-
 create table registration(
 reg_id int auto_increment not null,
 userid int,
-password varchar(128) not null,
-primary key(reg_id),
-foreign key (userid) references users(userid) on update cascade on delete cascade
+joindate date,
+primary key (reg_id)
 );
 
+create table users(
+userid int auto_increment not null,
+fname varchar(70),
+lname varchar(70),
+password varchar(128),
+primary key(userid)
+);
 
 create table users_info(
 userid int,
+email varchar(128),
 dob date,
-primary key(userid),
-foreign key(userid) references users(userid) on update cascade on delete cascade 
-);
-
-create table users_contact(
-userid int,
-email varchar(100),
-primary key(userid),
-foreign key(userid) references users(userid) on update cascade on delete cascade 
+primary key (userid),
+foreign key (userid) references users(userid) on update cascade on delete cascade
 );
 
 create table profile(
 userid int,
 user_status varchar(100),
 profile_pic varchar(128),
-primary key (userid),
-foreign key(userid) references users(userid) on update cascade on delete cascade 
+primary key(userid),
+foreign key (userid) references users(userid) on update cascade on delete cascade
 );
 
 create table groups(
-groupid int NOT NULL AUTO_INCREMENT,
-group_name varchar(50),
+groupid int auto_increment not null,
+groupname varchar(50),
 primary key(groupid)
 );
 
 create table comments(
-commentid int NOT NULL AUTO_INCREMENT,
+commentid int auto_increment not null,
 content varchar(200),
 comment_likes int,
 primary key(commentid)
 );
 
+create table comment_likes(
+commentid int,
+userid int,
+primary key (commentid, userid),
+foreign key (userid) references users(userid) on update cascade on delete cascade,
+foreign key (commentid) references comments(commentid) on update cascade on delete cascade
+);
+
 create table post(
-postid int NOT NULL AUTO_INCREMENT,
+postid int auto_increment not null,
 title varchar(128),
 post_type varchar(50),
-primary key(postid)
+primary key (postid)
+);
+
+create table post_likes(
+userid int,
+postid int,
+primary key (userid, postid),
+foreign key (userid) references users(userid) on update cascade on delete cascade,
+foreign key (postid) references post(postid) on update cascade on delete cascade
 );
 
 create table post_content(
 postid int,
 image_path varchar(128),
-text_body varchar(200),
+text_body varchar(250),
 post_like int,
-primary key(postid)
+primary key (postid),
+foreign key (postid) references post(postid) on update cascade on delete cascade
 );
 
 create table group_post(
 gpostid int auto_increment not null,
 title varchar(128),
 g_post_type varchar(50),
-primary key(gpostid)
+primary key (gpostid)
 );
 
 create table gpost_content(
 gpostid int,
 g_image_path varchar(128),
-text_body varchar(200),
+text_body varchar(250),
 gpost_like int,
 primary key (gpostid),
+foreign key (gpostid) references group_post(gpostid) on update cascade on delete cascade
+);
+
+create table gpost_likes(
+userid int,
+gpostid int,
+primary key(userid,gpostid),
 foreign key (gpostid) references group_post(gpostid) on update cascade on delete cascade
 );
 
@@ -86,16 +101,6 @@ create table creates(
 userid int,
 postid int,
 date_created date,
-primary key(postid),
-foreign key (userid) references users(userid) on update cascade on delete cascade,
-foreign key (postid) references post(postid) on update cascade on delete cascade
-);
-
-create table comments_on(
-userid int,
-postid int,
-commentid int,
-date_commented date,
 primary key(userid),
 foreign key (userid) references users(userid) on update cascade on delete cascade,
 foreign key (postid) references post(postid) on update cascade on delete cascade
@@ -108,6 +113,16 @@ type varchar(50),
 primary key(friend, friend_owner),
 foreign key (friend) references users(userid) on update cascade on delete cascade,
 foreign key (friend_owner) references users(userid) on update cascade on delete cascade
+);
+
+create table comments_on(
+userid int,
+postid int,
+commentid int,
+date_commented date,
+primary key(userid, postid, commentid),
+foreign key (userid) references users(userid) on update cascade on delete cascade,
+foreign key (postid) references post(postid) on update cascade on delete cascade
 );
 
 create table add_editors_group(
