e5694cf6e72f5bd90b147cb7c5fbb95991e083ac
Kenardo <headnoegoodagain@hotmail.com>
2014-04-15 17:04:19 -0500

    friends work

--- friends.php
+++ friends.php
@@ -1,7 +1,34 @@
 <?php
 include('header.html');
+include('query.php');
 ?>
+<br />
+<br />
 
+<!-- Jumbotron -->
+      
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Friends</h2>
+		  <?php
+			$tempfriend = mysql_query($friendsql);
+			//$gpdatastore = mysql_fetch_assoc($tempgpost);
+		
+			while ($frienddatastore = mysql_fetch_array($tempfriend))
+			{
+				//echo "<p><?$gpdatastore['title']
+				//echo $gpdatastore['title'];
+				?>
+			
+				<div class="gpostbox">
+				<p id="gposttitle"> <?= $frienddatastore['fname'] ?> <?= $frienddatastore['lname'] ?>
+				</div>
+				<br />
+				<?php
+			}
+		  ?>
 
 
 </div>
