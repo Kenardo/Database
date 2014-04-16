4aebbce83fcf474eca5dbbb59dbe7fd4fa56efb5
stewiejnr <stewie920@gmail.com>
2014-04-15 20:43:01 -0500

    post now works

--- post.php
+++ post.php
@@ -1,23 +1,88 @@
 <?php
 include('header.html');
+include('query.php');
+
+	if(isset($_POST['save'])) {
+		$errors = "";
+		$title = strtolower(stripslashes($_POST['ptitle']));
+		$content = strtolower(stripslashes($_POST['textbox']));
+		$ptype = 'text';
+		
+		if(mysql_query('INSERT INTO post (title, text_body, post_type) VALUES ("'.$title.'","'.$content.'","'.$ptype.'")'))
+		{
+			?>
+			<div class="message">Post successfully saved<br />
+			Make another <a href="post.php">Post</a></div>   
+			<?php	
+		}
+		else {
+			$errors .= "Post was not saved";
+		}
+		
+		$allowedExts = array("gif", "jpeg", "jpg", "png");
+		$temp = explode(".", $_FILES["file"]["name"]);
+		$extension = end($temp);
+		if ((($_FILES["file"]["type"] == "image/gif")
+		|| ($_FILES["file"]["type"] == "image/jpeg")
+		|| ($_FILES["file"]["type"] == "image/jpg")
+		|| ($_FILES["file"]["type"] == "image/pjpeg")
+		|| ($_FILES["file"]["type"] == "image/x-png")
+		|| ($_FILES["file"]["type"] == "image/png"))
+		&& ($_FILES["file"]["size"] < 20000000)
+		&& in_array($extension, $allowedExts))
+		  {
+		  if ($_FILES["file"]["error"] > 0)
+			{
+			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+			}
+		  else
+			{
+			//echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+			//echo "Type: " . $_FILES["file"]["type"] . "<br>";
+			//echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+			//echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+
+			if (file_exists("postpic/" . $_FILES["file"]["name"]))
+			  {
+			  echo $_FILES["file"]["name"] . " already exists. ";
+			  }
+			else
+			  {
+			  move_uploaded_file($_FILES["file"]["tmp_name"],
+			  "postpic/" . $_FILES["file"]["name"]);
+				 echo "<p class=\"message\">Picture uploaded successfully</p>";
+			  //echo "Stored in: " . "postpic/" . $_FILES["file"]["name"];
+			  }
+			}
+		  }
+		else
+		  {
+		  echo "Invalid file";
+		  }
+	}
+	
 ?>
 <br />
 <br />
-<form name ="input" action ="" method = "post">
-
-		<label class="left" for="title"><strong>Post Title:</strong></label><br/>
-		<label class="column"><input type="text" size="30" maxlength="16" id="g_title" name="g_title"></label><br /><br />
-		 
-				
+	<form name ="input" action ="" method = "post" enctype="multipart/form-data">
 		<br />
+		<label class="left" for="ptitle"><strong>Post Title:</strong></label>
+		<label class="column"><input type="text" size="30" id="ptitle" name="ptitle"></label><br /><br />
+	
 		<br />
 		<label class="left" for="textbox"><strong>Write Post below:</strong></label><br />
 		<label class="column"><textarea name = "textbox" rows ="4" cols ="30">
 		
 		</textarea>
 		</label><br /><br />
+		<label for="file">Upload photo:</label>
+		<input type="file" name="file" id="file"><br>
+		<!-- <input class="btn btn-info" type="submit" name="submit" value="Upload">  -->
 		
 		<button name="save" class="btn btn-info" type="submit">Save</button>
+		
+	<!-- </form>
+	<form action="upload_file.php" method="post" enctype="multipart/form-data"> -->
 		
 	</form>
 
--- query.php
+++ query.php
@@ -1,18 +1,18 @@
 <?php
 
-//HOME
+/* home queries */
 $gpostsql = "SELECT gp.gpostid,title, g_post_type, g_image_path, text_body, gpost_like  from group_post AS gp JOIN gpost_content AS gpc ON gp.gpostid=gpc.gpostid";
 $postsql = "SELECT pc.postid,title, post_type, image_path, text_body, post_like  from post AS p JOIN post_content AS pc ON p.postid=pc.postid"; 
+
+/* INSERT queries, it is used in post.php 
+
+INSERT INTO post (title, text_body, post_type) VALUES ("'.$title.'","'.$content.'","'.$ptype.'")
+
+*/
+
+/* admin queries */
 $friendsql = 'SELECT fname, lname from users WHERE userid IN (SELECT friend FROM friend_of WHERE friend_owner="'.$_SESSION['userid'].'")';
-
-//INSERT commands
-$postinsertsql = 'INSERT INTO post (title, text_body, post_type) VALUES ("'.$title.'","'.$content.'","text")';
-
-//ADMIN
-$allfriendsql = 'SELECT fname, lname from users WHERE userid IN (SELECT friend FROM friend_of WHERE friend_owner= "'.$uid.'"")';
-$commentsql = 'SELECT content from comments where commentid IN (SELECT commentid FROM comments_on WHERE userid="'.$uid.'")';
-$allpostsql = 'SELECT title from post where postid in (SELECT postid FROM creates where userid = "'.$uid.'") ';
-
-$usersql ='SELECT fname, lname from users';
 $usercountsql = 'SELECT COUNT(userid) FROM users';
+$commentsql = 'SELECT content from comments where commentid IN (SELECT commentid FROM comments_on WHERE userid="'.$_SESSION['userid'].'")';
+
 ?>
