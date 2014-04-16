de18f9adf431c268b6bc8b412885f8109d5daded
stewiejnr <stewie920@gmail.com>
2014-04-16 02:50:02 -0500

    new

--- query.php
+++ query.php
@@ -4,11 +4,12 @@
 $gpostsql = "SELECT gp.gpostid,title, g_post_type, g_image_path, text_body, gpost_like  from group_post AS gp JOIN gpost_content AS gpc ON gp.gpostid=gpc.gpostid";
 $postsql = "SELECT pc.postid,title, post_type, image_path, text_body, post_like  from post AS p JOIN post_content AS pc ON p.postid=pc.postid"; 
 
+/* get which group the user belongs to 
+SELECT groupname from groups where groupid in (SELECT groupid FROM add_to_group WHERE userid="'.$_SESSION['userid'].'")';
+*/
 
 /* INSERT queries, it is used in post.php 
-
 INSERT INTO post (title, text_body, post_type) VALUES ("'.$title.'","'.$content.'","'.$ptype.'")
-
 */
 
 /* INSERT queries, it is used in post.php
@@ -17,26 +18,18 @@ INSERT INTO post (postid, title, text_body, post_type) VALUES ("'.$newpostid.'",
 
 */
 
-
 /* INSERT queries, it is used in register.php 
-
 insert into users(userid, fname, lname, password) values ("'.$id.'","'.$fn.'", "'.$ln.'","'.$password.'")
 INSERT INTO users_info(userid, email, dob) VALUES ("'.$id.'","'.$em.'","'.$dob.'")
-
 */
 
 /* INSERT queries, it is used in result.php
-
 SELECT fname, lname from users WHERE userid IN (SELECT friend FROM friend_of WHERE friend_owner= $id)
-
 */
 
 /* Stored procedure call, it is used in profile.php
-
 call GetAge('$id')
-
 */
-
 
 
 /* admin queries */
